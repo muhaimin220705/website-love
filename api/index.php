1c64a8feeb78d2ab75cf2c9952e4b559a59c4866
AMIL <muhaiminazza4@gmail.com>
2026-03-16 16:12:15 +0700

    Setup Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+
+// 1. Arahkan ke file autoload milik Laravel
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Jalankan bootstrap Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 3. Tangani permintaan yang masuk melalui public/index.php
 require __DIR__ . '/../public/index.php';
